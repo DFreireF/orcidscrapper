5f0256ca80d8466671fc224c9d79aae96f24545a
DFreireF <D.FreireFernandez@gsi.de>
2023-12-08 20:40:51 +0100

    toml ,vie 08 dic 2023 20:40:51 CET

--- orcidscrapper/__main__.py
+++ orcidscrapper/__main__.py
@@ -1,28 +1,47 @@
 import requests
+import orcid
+import toml
+import argparse
 
-def get_orcid_number(researcher_name, affiliation):
-    search_url = f"https://orcid.org/orcid-search/search?queryText={researcher_name}+affiliation:{affiliation}&rows=1"
-    headers = {"Accept": "application/json"}
-
+def load_config(config_path):
     try:
-        response = requests.get(search_url, headers=headers)
-        response.raise_for_status()
-
-        data = response.json()
-        if "result" in data and data["result"]:
-            return data["result"][0]["orcid-identifier"]["path"]
-        else:
-            return None
-    except Exception as e:
-        print(f"Error: {e}")
-        print(f"Response content: {response.content.decode('utf-8')}")
+        with open(config_path, 'r') as config_file:
+            config = toml.load(config_file)
+        return config
+    except FileNotFoundError:
+        print(f"Error: Config file '{config_path}' not found.")
         return None
 
+def controller(tomlpath, researcher_name):
+
+    if tomlpath:
+        config = load_config(tomlpath)
+
+        if config:
+            # Use the values from the config
+            Client_ID = config.get('Client_ID', '')
+            Client_secret = config.get('Client_secret', '')
+
+    else:
+        print("Error: Please provide a path to the TOML configuration file using --tomlpath.")
+
+    api = orcid.PublicAPI(Client_ID, Client_secret)
+    search_token = api.get_search_token_from_orcid()
+    search_results = api.search(researcher_name, access_token=search_token)
+    return search_results
+
 def main():
+    scriptname = 'orcID Scrapper' 
+    parser = argparse.ArgumentParser()
+
+    # Main Arguments
+    parser.add_argument('--tomlpath', type=str, help='Path to the TOML configuration file')
+    args = parser.parse_args()
+
     researcher_name = input("Enter researcher's name: ")
     affiliation = input("Enter affiliation: ")
 
-    orcid_number = get_orcid_number(researcher_name, affiliation)
+    orcid_number = controller(args.tomlpath, researcher_name)
 
     if orcid_number:
         print(f"ORCID for {researcher_name} with affiliation {affiliation}: {orcid_number}")
@@ -31,3 +50,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+    
